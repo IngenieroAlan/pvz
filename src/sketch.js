215c7bdf701eb6b1800cb7f680ade21c287bfb43
IngenieroAlan <brandon.rod182@gmail.com>
2024-05-09 11:47:25 -0700

    Menu music implemented & new assets added

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,12 @@
 let menu;
 let font;
 let menuBackground;
+let menuSound;
+let button;
 
 function preload() {
+    soundFormats('mp3', 'ogg');
+    menuSound = loadSound('public/assets/music/main_menu_soundtrack.ogg');
     menuBackground = loadImage("public/assets/images/menuBackground.png");
     font = loadFont('public/assets/fonts/Samdan.ttf');
 }
@@ -10,6 +14,12 @@ function preload() {
 function setup() {
     createCanvas(900, 600);
     menu = true;
+
+    // Crear el botón
+    button = createButton('Play music');
+    button.position(width - 80, 20);
+    button.mousePressed(toggleMusic);
+
     if (menu == true) {
         image(menuBackground, 0, 0, 900, 600);
         filter(BLUR, 3);
@@ -20,5 +30,16 @@ function setup() {
     }
 }
 
+function toggleMusic() {
+    if (menuSound.isPlaying()) {
+        menuSound.stop();
+    } else {
+        menuSound.play();
+    }
+}
+
 function draw() {
+    if(!menu){
+        menuSound.stop();
+    }
 }
